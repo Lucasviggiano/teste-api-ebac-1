0af104a90ab1dcbf97dc174dbff00d58b15dffe6
lucas <viggiano.lucas@gmail.com>
2024-07-28 16:23:53 -0300

    segundo commit

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
 
 describe('Testes da Funcionalidade Usuários', () => {
 
@@ -8,23 +9,55 @@ describe('Testes da Funcionalidade Usuários', () => {
 
   it('Deve listar usuários cadastrados', () => {
     //TODO: 
+    cy.request({
+      method: 'GET',
+      url: 'usuarios'
+    }).then((response) => {
+      expect(response.status).equal(200)
+      expect(response.body).to.have.property('usuarios')
+      
+    })
   });
+
 
   it('Deve cadastrar um usuário com sucesso', () => {
     //TODO: 
+    cy.cadastrarUsuarios(faker.person.fullName(), faker.internet.email(), 'teste', 'true')
+      .then((response) => {
+
+        expect(response.status).equal(201)
+        expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+      });
+
   });
+
 
   it('Deve validar um usuário com email inválido', () => {
     //TODO: 
-  });
-
-  it('Deve editar um usuário previamente cadastrado', () => {
-    //TODO: 
-  });
-
-  it('Deve deletar um usuário previamente cadastrado', () => {
-    //TODO: 
-  });
+    cy.cadastrarUsuarios(faker.person.fullName(), 'Pierre_Hills@yahoo.com', 'teste', 'true')
+      .then((response) => {
+        expect(response.status).equal(400)
+        expect(response.body.message).to.equal('Este email já está sendo usado')
+      });
 
 
-});
+    it('Deve editar um usuário previamente cadastrado', () => {
+      //TODO: 
+
+
+    });
+
+    it('Deve deletar um usuário previamente cadastrado', () => {
+      //TODO: 
+
+
+
+
+
+
+
+    });
+
+  })
+
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,45 @@
+
+
 Cypress.Commands.add('token', (email, senha) => {
     cy.request({
         method: 'POST',
         url: 'login',
         body: {
             "email": email,
-            "password": senha 
+            "password": senha
         }
     }).then((response) => {
         expect(response.status).to.equal(200)
         return response.body.authorization
     })
- })
+})
 
- Cypress.Commands.add('cadastrarProduto' , (token, produto, preco, descricao, quantidade) =>{
+Cypress.Commands.add('cadastrarProduto', (token, produto, preco, descricao, quantidade) => {
+    
     cy.request({
-        method: 'POST', 
+        method: 'POST',
         url: 'produtos',
-        headers: {authorization: token}, 
+        headers: { authorization: token },
         body: {
             "nome": produto,
             "preco": preco,
             "descricao": descricao,
             "quantidade": quantidade
-          }, 
-          failOnStatusCode: false
+        },
+        failOnStatusCode: false
     })
- })
+})
+
+Cypress.Commands.add('cadastrarUsuarios', (username, usermail, senha, administrador) => {
+    cy.request({
+        method: 'POST',
+        url: 'usuarios',
+        body: {
+            "nome": username,
+            "email": usermail,
+            "password": senha,
+            "administrador": administrador
+        },  
+        failOnStatusCode: false
+    })
+})
